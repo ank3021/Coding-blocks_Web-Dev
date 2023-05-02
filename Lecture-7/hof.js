b7497400b2f2a73a3d57cc26ee7f83da9f7f3e3d
ankit <mankitkashyapr@gmail.com>
2023-05-02 09:40:21 +0530

    "Higher Order functions"

--- Lecture-7/hof.js
+++ Lecture-7/hof.js
@@ -1,9 +1,15 @@
+
+//creategreeter is an higher order function, as it is returning function greet as an entity.
 function creategreeter(greeting){
-         function greet(name)
+         function greet(name)//greet is also an higher order function, as it has the function getname as the input parameter.
          {
             console.log(greeting, name);
          }
      return greet;
+}
+
+function getname(){
+    return document.getElementById("namebox").value;
 }
 
 
@@ -11,3 +17,9 @@ let g1 = creategreeter('Good Morning');
 let g2 = creategreeter('Good Evening');
 console.log(g1("Ankit"));
 console.log(g2("Nikhil"));
+
+
+/*In javaScript, functions are first class citizens which means that they can be treated like variables. Functions can be passed by higher order functions, can be used as an
+argument and updated.*/
+/*return function_name --> It returns the function (complete function body)
+  return function_name() --> It returns the value passed by the function*/ 
